613f98e44c974a92f888fbbce7f5eca2e88bb15a
Nelson Diaz <abnerdc86@gmail.com>
2025-08-17 22:18:13 -0400

    Add About page and Link component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 import HomePage from './pages/home';
+import AboutPage from './pages/about';
+import Header from './components/Header';
 import { Routes, Route } from 'react-router';
 
 const API_URL = import.meta.env.VITE_API_URL;
@@ -33,27 +35,31 @@ const App = () => {
   }, [limit]);
 
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <HomePage
-            coins={coins}
-            loading={loading}
-            error={error}
-            limit={limit}
-            filter={filter}
-            sortBy={sortBy}
-            setCoins={setCoins}
-            setLoading={setLoading}
-            setError={setError}
-            setLimit={setLimit}
-            setFilter={setFilter}
-            setSortBy={setSortBy}
-          />
-        }
-      />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <HomePage
+              coins={coins}
+              loading={loading}
+              error={error}
+              limit={limit}
+              filter={filter}
+              sortBy={sortBy}
+              setCoins={setCoins}
+              setLoading={setLoading}
+              setError={setError}
+              setLimit={setLimit}
+              setFilter={setFilter}
+              setSortBy={setSortBy}
+            />
+          }
+        />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </>
   );
 };
 
